8ae01a9b6168a38cfb7a2e12f3f536b5400a412c
SehwaajSingh <sehwaajcollege@gmail.com>
2026-05-29 19:06:01 +0530

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Desktop } from '@/components/desktop/Desktop';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen w-screen overflow-hidden bg-black select-none">
+      <Desktop />
+    </main>
+  );
+}
